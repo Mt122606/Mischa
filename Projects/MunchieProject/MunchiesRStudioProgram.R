040a1b973ba38895c5e8d3b9b587749324c5c088
Mt122606 <108757589+Mt122606@users.noreply.github.com>
2023-10-07 16:05:47 -0500

    Create MunchiesRStudioProgram.R

---
+++ Projects/MunchieProject/MunchiesRStudioProgram.R
@@ -0,0 +1,15 @@
+### Major Munchies 
+## Revenue Exploration
+
+#Upload Data
+library(readxl)
+Revenue <- read_excel("../MunchieProject/Data/Revenue.xlsx")
+View(Revenue)
+
+#Preparation 
+'Make Dummy codes for items'
+'Code to produce monthly revenue statement'
+'Code to produce monthly revenue statement'
+'Code to produce weekly revenue statement'
+'Code to view linear regresstion model'
+library(dplyr)
